f1830e4975592a3fa9656145d5f8818de2d0ace6
chuckling1 <chuckling1@gmail.com>
2022-02-19 15:09:05 -0500

    Added Play, Pause, and Slider functions.

--- TrackpointsApp/App.tsx
+++ TrackpointsApp/App.tsx
@@ -1,10 +1,21 @@
 /* eslint-disable prettier/prettier */
 import React, { useEffect, useState } from 'react';
 import { Button, Text } from 'react-native';
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, { Capability, Event, State } from 'react-native-track-player';
+import { useProgress, useTrackPlayerEvents } from 'react-native-track-player/lib/hooks';
+import Slider from '@react-native-community/slider';
 
-//function to initialize the Track Player 
 const trackPlayerInit = async () => {
+  TrackPlayer.updateOptions({
+    stopWithApp: true,
+    capabilities: [
+      Capability.Play,
+      Capability.Pause,
+      Capability.JumpForward,
+      Capability.JumpBackward,
+    ],
+  });
+
   await TrackPlayer.setupPlayer();
   await TrackPlayer.add({
     id: '1',
@@ -15,36 +26,77 @@ const trackPlayerInit = async () => {
     artist: 'Rohan Bhatia',
     artwork: 'https://picsum.photos/100',
   });
+
   return true;
 };
 
 const App = () => {
-
-  //state to manage whether track player is initialized or not
   const [isTrackPlayerInit, setIsTrackPlayerInit] = useState(false);
+  const [isPLaying, setIsPlaying] = useState(false);
+  const [sliderValue, setSliderValue] = useState(0);
+  const [isSeeking, setIsSeeking] = useState(false);
+  const { position, duration } = useProgress(100);
 
-  //initialize the TrackPlayer when the App component is mounted
   useEffect(() => {
     const startPlayer = async () => {
       let isInit = await trackPlayerInit();
       setIsTrackPlayerInit(isInit);
-    }
+    };
+
     startPlayer();
   }, []);
 
-  //start playing the TrackPlayer when the button is pressed 
   const onButtonPressed = () => {
-    TrackPlayer.play();
+    if (!isPLaying) {
+      TrackPlayer.play();
+      setIsPlaying(true);
+    } else {
+      TrackPlayer.pause();
+      setIsPlaying(false);
+    }
   };
 
+  const slidingStarted = () => {
+    setIsSeeking(true);
+  };
+
+  const slidingCompleted = async (value: number) => {
+    await TrackPlayer.seekTo(value * duration);
+    setSliderValue(value);
+    setIsSeeking(false);
+  };
+
+  useEffect(() => {
+    if (!isSeeking && position && duration) {
+      setSliderValue(position / duration);
+    }
+  }, [position, duration, isSeeking]);
+
+  useTrackPlayerEvents([Event.PlaybackState], event => {
+    if (event.state === State.Playing) {
+      setIsPlaying(true);
+    } else {
+      setIsPlaying(false);
+    }
+  });
 
   return (
     <>
       <Text>Music Player</Text>
       <Button
-        title="Play"
+        title={isPLaying ? 'Pause' : 'Play'}
         onPress={onButtonPressed}
         disabled={!isTrackPlayerInit}
+      />
+      <Slider
+        style={{ width: 400, height: 40 }}
+        minimumValue={0}
+        maximumValue={1}
+        value={sliderValue}
+        minimumTrackTintColor="#111000"
+        maximumTrackTintColor="#000000"
+        onSlidingStart={slidingStarted}
+        onSlidingComplete={slidingCompleted}
       />
     </>
   );
